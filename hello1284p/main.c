c9f7e734d17dd03b3794432435c5d3eddd104efd
Christophe VG <xtof@astroboy.local>
2013-11-03 20:04:52 +0100

    trimmed hello1284p example to a minimum

--- hello1284p/main.c
+++ hello1284p/main.c
@@ -1,7 +1,5 @@
-// hello1284p is a simple program that augments a counter and blinks the amount
-// of that counter. It also writes the memory address of the counter to the
-// USART. It was written to experiment with avarice/avr-gdb to try and access
-// the data memory of a running system.
+// This is a simple program that augments a counter and prints this value to a
+// terminal via its USART port.
 //
 // author: Christophe VG
 
@@ -9,9 +7,7 @@
 #include <util/delay.h>
 
 #include "../avr/avr.h"
-#include "../avr/bool.h"
-
-#define STATUS_LED 6
+#include "../avr/serial.h"    // wires printf to the USART port
 
 unsigned int counter = 0;
 
@@ -19,19 +15,10 @@ int main(void) {
   avr_init();
   serial_init();
 
-  printf("counter is at %x\n", &counter);
-
-  while(TRUE) {
+  while(1) {
     counter++;
-
     printf("counter = %i\n", counter);
-
-    for(int i=counter;i>0;i--) {
-      avr_set_bit  (PORTD, STATUS_LED);  _delay_ms(200);
-      avr_clear_bit(PORTD, STATUS_LED);  _delay_ms(200);
-    }
-    
-    _delay_ms(2000);
+    _delay_ms(1000);
   }
 
   return(0);
