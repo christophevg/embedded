5d459173bec657de6974982d3cb34d1dceb2d506
Christophe VG <xtof@astroboy.local>
2013-12-01 16:38:00 +0100

    implemented buffered interrupt driven receiving continued dual USART support for XBee started work on xt0f-004 driver

--- xt0f-004/main.c
+++ xt0f-004/main.c
@@ -28,29 +28,25 @@ void send_str(const char *string);
 
 // configuration of external components
 
-#define STATUS_LED_PORT    PORTD  // PD3
-#define STATUS_LED_PIN     3
-#define OPERATION_LED_PORT PORTD  // PD4
-#define OPERATION_LED_PIN  4
-#define ALERT_LED_PORT     PORTD  // PD5     
+#define STATUS_LED_PORT    PORTD  // PD6
+#define STATUS_LED_PIN     6
+// #define OPERATION_LED_PORT PORTD  // PD4
+// #define OPERATION_LED_PIN  4
+// #define ALERT_LED_PORT     PORTD  // PD5     
 
-#define TEMP_SENSOR_PORT   DDRC   // PC4
-#define TEMP_SENSOR_PIN    5
-#define LIGHT_SENSOR_PORT  DDRC   // PC5
-#define LIGHT_SENSOR_PIN   4
+// #define TEMP_SENSOR_PORT   DDRC   // PC4
+// #define TEMP_SENSOR_PIN    5
+// #define LIGHT_SENSOR_PORT  DDRC   // PC5
+// #define LIGHT_SENSOR_PIN   4
 
-#define VALUES_COUNT 4            // 2 bytes temperature & light
-
-// we use both USARTs
-#define SERIAL_ON_USART0
-#define XBEE_ON_USART1
+// #define VALUES_COUNT 4            // 2 bytes temperature & light
 
 // network config
 #define DESTINATION XBEE_COORDINATOR
 
 int main(void) {
-  uint16_t reading;               // the 16-bit reading from the ADC
-  uint8_t  values[VALUES_COUNT];  // the bytes containing the readings
+  // uint16_t reading;               // the 16-bit reading from the ADC
+  // uint8_t  values[VALUES_COUNT];  // the bytes containing the readings
 
   avr_init();                     // initialise MCU
   avr_set_bit(STATUS_LED_PORT,    // turn on the red status led
@@ -58,37 +54,47 @@ int main(void) {
   avr_adc_init();                 // initialize the ADC for normal readings
   serial_init();                  // initialize use of serial port(s)
 
+  printf("------------------------------------------------------\n");
   printf("serial initialized\n"); // announce boot process via serial
 
   sleep_init();                   // we're using power-down-style sleeping
 
-  avr_clear_bit(TEMP_SENSOR_PORT, // prepare sensor pins for input
-                TEMP_SENSOR_PIN );
-  avr_clear_bit(LIGHT_SENSOR_PORT,
-                LIGHT_SENSOR_PIN);
+  // avr_clear_bit(TEMP_SENSOR_PORT, // prepare sensor pins for input
+  //               TEMP_SENSOR_PIN );
+  // avr_clear_bit(LIGHT_SENSOR_PORT,
+  //               LIGHT_SENSOR_PIN);
+
+  xbee_init();                    // initialize use of XBee module
+
+  printf("xbee initialized\n");
 
   xbee_wait_for_association();    // wait until the network is available
 
-  printf("xbee initialized\n");   // announce boot process via serial
-  send_str("xbee initialized\n"); // announce boot process via xbee
+  printf("xbee associated\n");    // announce boot process via serial
+  send_str("xbee associated\n");  // announce boot process via xbee
 
   while(TRUE) {
-    wakeup();
+    // wakeup();
 
-    // temperature
-    reading = avr_adc_read(TEMP_SENSOR_PIN);
-    values[1] = reading & 0x00FF;
-    values[2] = (reading >> 8) & 0x00FF;
+    // // temperature
+    // reading = avr_adc_read(TEMP_SENSOR_PIN);
+    // values[1] = reading & 0x00FF;
+    // values[2] = (reading >> 8) & 0x00FF;
+    // 
+    // // light
+    // reading = avr_adc_read(LIGHT_SENSOR_PIN);
+    // values[3] = reading & 0x00FF;
+    // values[4] = (reading >> 8 ) & 0x00FF;
+    // 
+    // // print it to the serial
+    // send_bytes(values, VALUES_COUNT);
 
-    // light
-    reading = avr_adc_read(LIGHT_SENSOR_PIN);
-    values[3] = reading & 0x00FF;
-    values[4] = (reading >> 8 ) & 0x00FF;
+    printf("step\n");
+    send_str("step\n");
 
-    // print it to the serial
-    send_bytes(values, VALUES_COUNT);
+    _delay_ms(1000L);
 
-    sleep();
+    // sleep();
   }
 
   return(0);
@@ -102,7 +108,7 @@ void send_bytes(uint8_t *bytes, uint8_t size) {
   frame.address     = XB_COORDINATOR;
   frame.nw_address  = XB_NW_ADDR_UNKNOWN;
   frame.radius      = XB_MAX_RADIUS;
-  frame.options     = XN_OPT_NONE;
+  frame.options     = XB_OPT_NONE;
   frame.data        = bytes;
 
   xbee_send(&frame);
@@ -113,7 +119,7 @@ void send_str(const char *string) {
 }
 
 void wakeup(void) {
-  avr_set_bit(PORTB, SENSOR_VCC);   // give power to sensors
+  // avr_set_bit(PORTB, SENSOR_VCC);   // give power to sensors
   _delay_ms(10);                    // time to rise - TODO: how o this CLEANLY?
   avr_adc_init();                   // re-initialize the ADC
                                     // TODO: move this elsewhere once I
@@ -124,6 +130,6 @@ void wakeup(void) {
 
 void sleep(void) {
   xbee_sleep();                     // put XBee to sleep
-  avr_clear_bit(PORTB, SENSOR_VCC); // revoke power to sensors
-  sleep_ms(59000L);                 // power-down the MCU for 59 seconds
+  // avr_clear_bit(PORTB, SENSOR_VCC); // revoke power to sensors
+  sleep_ms(1000L);                  // power-down the MCU for 1 second
 }
