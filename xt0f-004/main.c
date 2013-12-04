3794e102159c398e258e0f7fe1f8124e02f2da08
Christophe VG <xtof@astroboy.local>
2013-12-04 14:38:31 +0100

    changed VCC sensor pin

--- xt0f-004/main.c
+++ xt0f-004/main.c
@@ -32,7 +32,7 @@
 #define LIGHT_SENSOR_PORT  DDRA   // PA0
 #define LIGHT_SENSOR_PIN   0
 
-#define VCC_SENSOR_PORT    PORTB  // PB7
+#define VCC_SENSOR_PORT    PORTA  // PB7
 #define VCC_SENSOR_PIN     7
 
 // network config
