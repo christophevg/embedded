614a01429d27ac7a7514b263526375249b5f66c8
Christophe VG <contact@christophe.vg>
2015-08-16 17:57:59 +0200

    added draft support for an accelerometer

---
+++ accelerometer/main.c
@@ -0,0 +1,50 @@
+// 
+// author: Christophe VG <contact@christophe.vg>
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+
+#include "../moose/avr.h"
+#include "../moose/bool.h"
+#include "../moose/serial.h"
+
+/* configuration of external components */
+#define XYZ_PORT DDRA
+#define PIN_X    0  // PA0
+#define PIN_Y    1  // PA1
+#define PIN_Z    2  // PA2
+
+int main(void) {
+  avr_init();
+  serial_init();
+
+  // initialize the ADC for normal readings
+  avr_adc_init();
+
+  avr_clear_bit(XYZ_PORT, PIN_X);
+  avr_clear_bit(XYZ_PORT, PIN_Y);
+  avr_clear_bit(XYZ_PORT, PIN_Z);
+
+  printf("boot sequence completed...\n");
+
+  // the endless loop
+  uint16_t  x,  y,  z;
+  float    gx, gy, gz;
+  while(TRUE) {
+    x = avr_adc_read(PIN_X);
+    y = avr_adc_read(PIN_Y);
+    z = avr_adc_read(PIN_Z);
+
+    // scale to [-1,+1]
+    gx = (x / 1024.0 * 2) - 1;
+    gy = (y / 1024.0 * 2) - 1;
+    gz = (z / 1024.0 * 2) - 1;
+
+    printf("%f\t%f\t%f\n", gx, gy, gz);
+
+    _delay_ms(5);
+  }
+
+  return(0);
+}
---
+++ accelerometer/plot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+# this program plots the three axis of the accelerometer, but it's TOO SLOW for
+# real-time analysis.
+
+import matplotlib
+matplotlib.use('TKAgg')
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+import serial
+import string
+
+ser = serial.Serial("/dev/tty.usbserial-FTSJ84AI", 9600)
+
+def wait_for_nl():
+  ser.readline();
+
+def getXYZ():
+  while(True):
+    try:
+      (x, y, z) = string.split(ser.readline(), "\t")
+      return (float(x), float(y), float(z))
+    except: pass
+
+wait_for_nl() # to avoid reading a partial line already being sent
+
+x = np.arange(0, 100)
+y = np.zeros(100)
+
+fig, axes = plt.subplots(nrows=3)
+styles    = ['r-', 'g-', 'y-' ]
+
+def plot(ax, style):
+  ax.axis([0, 100, -1, 1])
+  return ax.plot(x, y, style, animated=True)[0]
+
+lines = [plot(ax, style) for ax, style in zip(axes, styles)]
+
+x_data = np.zeros(100)
+y_data = np.zeros(100)
+z_data = np.zeros(100)
+
+def animate(i):
+  global x_data, y_data, z_data
+  (xn,yn,zn) = getXYZ()
+  x_data = np.append(x_data, xn)[-100:]
+  y_data = np.append(y_data, yn)[-100:]
+  z_data = np.append(z_data, zn)[-100:]
+  lines[0].set_ydata(x_data)
+  lines[1].set_ydata(y_data)
+  lines[2].set_ydata(z_data)
+  return lines
+
+ani = animation.FuncAnimation(fig, animate, xrange(1, 200), 
+                              interval=0, blit=True)
+plt.show()
